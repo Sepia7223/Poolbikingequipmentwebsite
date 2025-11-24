ce6bb20ab641268164d19eef205ff8d7839b6c1b
Sepia7223 <1146920@student.uoc.cw>
2025-11-24 18:20:01 -0400

    Allow .tif assets in Vite build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@
 
   export default defineConfig({
     plugins: [react()],
+    assetsInclude: ['**/*.tif'],
     resolve: {
       extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
       alias: {
