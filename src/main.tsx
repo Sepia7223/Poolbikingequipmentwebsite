c72478365ba1bf2d8b56563f527d30e38ee68203
Jaydon <1146920@student.uoc.cw>
2026-09-13 14:09:20 -0400

    Apply final redesign polish styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,5 +2,6 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import "./redesign.css";
+import "./redesign-fixes.css";
 
 createRoot(document.getElementById("root")!).render(<App />);
